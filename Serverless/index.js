66fdde646e2fb0ca5d50c9c301d480c99f55764b
patil-pus <patil.pus@northeastern.edu>
2023-11-29 20:37:39 -0500

    minor changes

--- Serverless/index.js
+++ Serverless/index.js
@@ -7,10 +7,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 AWS.config.update({
-  region: 'us-east-1', 
-  
+  region: 'us-east-1'
 });
-const docClient = new AWS.DynamoDB.DocumentClient();
 
 async function isValidZipUrl(urlString) {
   try {
@@ -30,8 +28,9 @@ async function isValidZipUrl(urlString) {
   }
 }
 
+
 async function saveUrlToGCS(url, bucketName, fileName) {
-  console.log("this is the google access key", process.env.GOOGLE_ACCESS_KEY)
+  console.log("this is the google access key", process.env.GOOGLE_ACCESS_KEY);
   const googleCredentials = JSON.parse(process.env.GOOGLE_ACCESS_KEY);
   const storage = new Storage({ credentials: googleCredentials });
   const bucket = storage.bucket(bucketName);
@@ -40,65 +39,82 @@ async function saveUrlToGCS(url, bucketName, fileName) {
   return new Promise((resolve, reject) => {
     const stream = file.createWriteStream({
       metadata: {
-        contentType: 'text/html',
+        contentType: 'application/zip',
       },
     });
 
-    stream.on("error", (err) => reject(err));
-    stream.on("finish", () => resolve("URL saved to GCS"));
+    stream.on('error', (err) => reject(err));
+    stream.on('finish', async () => {
+      try {
+        await file.makePublic();
+        console.log('Upload successful and file is made public');
+        resolve('URL saved to GCS and made public');
+      } catch (err) {
+        console.error('Error making file public:', err);
+        reject(err);
+      }
+    });
 
-    stream.end(url);
+    axios
+      .get(url, { responseType: 'stream' })
+      .then((response) => {
+        response.data.pipe(stream);
+      })
+      .catch((error) => {
+        console.error('Error downloading file from URL:', error);
+        reject(error);
+      });
   });
-}
-
-async function saveEmailSent(emailId, emailDetails) {
-  const params = {
-    TableName: 'EmailsSent', 
-    Item: {
-      emailId: emailId, 
-      emailDetails: emailDetails, 
-      sentAt: new Date().toISOString()
-    }
-  };
-
-  try {
-    await docClient.put(params).promise();
-    console.log('Email record saved:', emailId);
-  } catch (error) {
-    console.error('Error saving email record:', error);
-  }
-}
+};
 
 export async function handler(event, context) {
+  const dynamoDB = new AWS.DynamoDB.DocumentClient();
   const snsMes = event.Records[0].Sns.Message;
   const messageData = JSON.parse(snsMes);
   const mail = messageData.Mail;
   const url = messageData.http;
-  const emailBody = `You can find the file here /${mail + new Date()}`;
+  const emailSubject = "Posted Submission";
   const mg = mailgun({
     apiKey: process.env.MAILGUN_API_KEY,
     domain: process.env.Domain_Name,
   });
-
-  await mg.messages().send({
-    from: "help@pushkarpatil.com",
-    to: mail,
-    subject: "Posted Submission",
-    text: emailBody,
-  });
-  await saveEmailSent(mail, { url: url, dateSent: new Date().toISOString() });
-  console.log("Email sent successfully");
-
   try {
     const isValid = await isValidZipUrl(url);
+    let emailBody, emailDetails;
+
     if (isValid) {
+      const date= Date.now().toString();
       console.log("The URL points to a ZIP file.");
-      const fileName = url.split('/').pop(); // Extracts file name from URL
-      await saveUrlToGCS(url, process.env.BUCKET_NAME, fileName + new Date());
-      console.log("URL saved successfully");
+      const fileName = url.split('/').pop();
+      const file_name=fileName+date
+      await saveUrlToGCS(url, process.env.BUCKET_NAME, file_name);
+      console.log("URL saved successfully",file_name);
+      emailBody = `Your submission is processed successfully. You can find the file here: https://storage.googleapis.com/${process.env.BUCKET_NAME}/${file_name}`;
+      emailDetails = "Success, The URL points to a ZIP file.";
+      console.log("Email sent successfully");
     } else {
       console.log("The URL does not point to a ZIP file.");
+      emailBody = `Your submission failed. The provided URL does not point to a valid ZIP file.`;
+      emailDetails = "Failed. The URL does not point to a ZIP file.";
     }
+
+    await mg.messages().send({
+      from: "help@pushkarpatil.com",
+      to: mail,
+      subject: emailSubject,
+      text: emailBody,
+    });
+    
+
+    // Update DynamoDB
+    const params = {
+      TableName: process.env.dynamoTableName,
+      Item: {
+        emailId: mail + new Date().toISOString(),
+        emailDetails: emailDetails,
+      },
+    };
+    await dynamoDB.put(params).promise();
 
     return {
       statusCode: 200,
